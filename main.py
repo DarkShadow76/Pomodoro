ecdef46a0375bfcf1bd19baef25a519eee88b723
DarkShadow76 <alonzotoricaceres@gmail.com>
2025-12-05 23:39:00 -0500

    Fixing count_down error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
-import colorgram
+##import colorgram
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,7 +13,7 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 timer = None
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
 
 def reset_timer():
@@ -22,6 +23,8 @@ def reset_timer():
     check_marks.config(text="")
     global reps
     reps = 0
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
@@ -42,11 +45,12 @@ def start_timer():
     else:
         count_down(work_sec)
         title_label.config(text="Work", fg=GREEN)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def count_down(count):
-
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
@@ -56,44 +60,67 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         global timer
-        timer = window.after(1000, count_down, count-1)
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ""
-        work_session = math.floor(reps/2)
+        work_session = math.floor(reps / 2)
         for _ in range(work_session):
             marks += ""
-        check_marks.config(text=marks)
+        check_marks.config(text="marks")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW,)
-window.after(1000, )
+window.config(
+    padx=20,
+    pady=20,
+    bg=YELLOW,
+)
+window.after(
+    1000,
+)
+##window.geometry("450x450")
 
-title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+window.resizable(False, False)
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 100))
 title_label.grid(column=1, row=0)
+
+##title_label.pack(pady=50)
 
 fg = GREEN
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
 canvas.grid(column=1, row=1)
 
-count_down(0)
-
-
-
-start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button = Button(
+    text="Start",
+    highlightthickness=0,
+    command=start_timer,
+    font=(FONT_NAME, 50)
+    )
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer())
+reset_button = Button(
+    text="Reset",
+    highlightthickness=0,
+    command=reset_timer,
+    font=(FONT_NAME, 50)
+    )
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
+
+count_down(0)
 
 window.mainloop()
